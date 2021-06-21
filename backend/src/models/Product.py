01ce4617af825ccd96f310999ba00aadadda3aa0
Pasquale Rombolà <pasquale-rombola@dedagroup.it>
2021-06-21 14:26:02 +0200

    add product name in model and migrate.

--- backend/src/models/Product.py
+++ backend/src/models/Product.py
@@ -5,6 +5,7 @@ class Product(db.Model):
 
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(64))
     quantity = db.Column(db.Integer)
     image_path = db.Column(db.String(64), unique=True, nullable=True)
@@ -13,6 +14,7 @@ class Product(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
+            "name": self.name,
             'description': self.description,
             'quantity': self.quantity,
             'image_path': self.image_path,
